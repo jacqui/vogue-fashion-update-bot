28ab77d0ba7374fdb972b090f3925cfae44e9d26
Jacqui Maher <jacqui.maher@condenastint.com>
2017-02-13 09:49:32 +0000

    fixed capistrano file (ssh issue)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "ec2-54-194-183-98.eu-west-1.compute.amazonaws.com", user: "ubuntu", roles: %w{app db web}
+# server "ec2-54-194-183-98.eu-west-1.compute.amazonaws.com", user: "ubuntu", roles: %w{app db web}
 
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
@@ -50,13 +50,12 @@ set :ssh_options, {
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+server "ec2-54-194-183-98.eu-west-1.compute.amazonaws.com",
+  user: "ubuntu",
+  roles: %w{web app},
+  ssh_options: {
+    user: "ubuntu", # overrides user setting above
+    keys: %w(/home/jmaher/.ssh/messenger-bot.pem),
+    forward_agent: true,
+    auth_methods: %w(publickey)
+  }
