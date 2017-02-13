406b6a4b6f6989f5ee3cffb92dd6c6bf869b54b5
Jacqui Maher <jacqui.maher@condenastint.com>
2017-02-13 15:06:22 +0000

    bugfix: message var

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   def self.log_it(received_message)
-    msg = Message.create(mid: message.messaging['message']['mid'], senderid: message.messaging['sender']['id'], seq: message.messaging['message']['seq'], sent_at: message.messaging['timestamp'], text: message.text)
+    msg = Message.create(mid: received_message.messaging['message']['mid'], senderid: received_message.messaging['sender']['id'], seq: received_message.messaging['message']['seq'], sent_at: received_message.messaging['timestamp'], text: received_message.text)
     puts "Stored a record of this message: #{msg.id}"
     return msg
   end
