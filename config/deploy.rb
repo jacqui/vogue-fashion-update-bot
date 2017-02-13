a0022340145b92f90de2df619ce5a13b3c916bb6
Jacqui Maher <jacqui.maher@condenastint.com>
2017-02-13 09:02:49 +0000

    Set git branch to master

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 set :migration_role, :app
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, "master"
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
