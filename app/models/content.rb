8d0ff11eb3bec1a674af6b891dc112fa20bc42d4
Jacqui Maher <jacqui.maher@condenastint.com>
2017-02-14 09:36:10 +0000

    Support changing the greeting text in Facebook

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,20 @@ class Content < ApplicationRecord
   validates :title, uniqueness: true, presence: true
   validates :label, uniqueness: true, presence: true
   validates :body, presence: true
+
+  after_save :update_facebook
+
+  include Facebook::Messenger
+
+  def update_facebook
+    case label
+    when "greeting"
+      Facebook::Messenger::Thread.set({
+        setting_type: 'greeting',
+        greeting: {
+          text: body
+        },
+      }, access_token: ENV['ACCESS_TOKEN'])
+    end
+  end
 end
