839e3ef00b5d7946f9c066602db97cc054b3a998
Jacqui Maher <jacqui.maher@condenastint.com>
2017-02-16 11:21:08 +0000

    Added more text to the british vogue response

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -69,7 +69,7 @@ if Rails.env.production?
             type: 'template',
             payload: {
               template_type: 'button',
-              text: 'British Vogue',
+              text: 'Visit British Vogue for fashion news, backstage photos, fashion trends, catwalk videos, supermodel interviews, beauty trends and celebrity party photos.',
               buttons: [{
                 type: "web_url",
                 url: "http://www.vogue.co.uk/" + Article::URL_TRACKING_PARAMS,
