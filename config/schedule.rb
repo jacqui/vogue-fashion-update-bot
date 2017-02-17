403c1d96664c346d87531126e80344f1ba3cbcd9
Jacqui Maher <jacqui.maher@condenastint.com>
2017-02-17 13:40:43 +0000

    Turned the scripts for shows, top stories back on

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,14 +2,14 @@ env :PATH, ENV['PATH']
 set :path, "/home/ubuntu/messenger_bot/current"
 set :output, {:error => '/home/ubuntu/messenger_bot/shared/log/cron_errors.log', :standard => '/home/ubuntu/messenger_bot/shared/log/cron_output.log'}
 
-# every 1.hour do
-#   rake "shows:major"
-# end
-# 
-# every 90.minutes do
-#   rake "shows:regular"
-# end
-# 
+every 10.minutes do
+  rake "shows:major"
+end
+ 
+every 8.minutes do
+  rake "shows:regular"
+end
+ 
 # every 15.minutes do
 #   rake "articles:subs"
 # end
@@ -18,8 +18,8 @@ set :output, {:error => '/home/ubuntu/messenger_bot/shared/log/cron_errors.log',
 #   rake "brands:populate"
 # end
 
-# every 6.hours do
-#   rake "articles:top"
-# end
+every 1.hour do
+  rake "articles:top"
+end
 
 # Learn more: http://github.com/javan/whenever
