77c3849a1fd4f31f07888600adc7000bb6a0d280
Jacqui Maher <jacqui.maher@condenastint.com>
2017-02-17 12:46:30 +0000

    Commented out cron scripts for now

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,24 +2,24 @@ env :PATH, ENV['PATH']
 set :path, "/home/ubuntu/messenger_bot/current"
 set :output, {:error => '/home/ubuntu/messenger_bot/shared/log/cron_errors.log', :standard => '/home/ubuntu/messenger_bot/shared/log/cron_output.log'}
 
-every 1.hour do
-  rake "shows:major"
-end
-
-every 90.minutes do
-  rake "shows:regular"
-end
-
-every 15.minutes do
-  rake "articles:subs"
-end
+# every 1.hour do
+#   rake "shows:major"
+# end
+# 
+# every 90.minutes do
+#   rake "shows:regular"
+# end
+# 
+# every 15.minutes do
+#   rake "articles:subs"
+# end
 
 # every 1.day do
 #   rake "brands:populate"
 # end
 
-every 6.hours do
-  rake "articles:top"
-end
+# every 6.hours do
+#   rake "articles:top"
+# end
 
 # Learn more: http://github.com/javan/whenever
