e4254c382ceb9b263399c0d59c6555e9632a0781
Jacqui Maher <jacqui.maher@condenastint.com>
2017-02-21 14:23:18 +0000

    Broadcast messages now support the generic template

--- app/models/broadcast.rb
+++ app/models/broadcast.rb
@@ -1,18 +1,54 @@
 class Broadcast < ApplicationRecord
   has_and_belongs_to_many :users
+  validates :title, presence: true, 
+    unless: Proc.new { |b| b.template.blank? || b.template == 'text' }
+  validates :link, presence: true, 
+    unless: Proc.new { |b| b.template.blank? || b.template == 'text' }
+  validates :button_text, presence: true, 
+    unless: Proc.new { |b| b.template.blank? || b.template == 'text' }
+  validates :image_url, presence: true, 
+    unless: Proc.new { |b| b.template.blank? || b.template == 'text' }
 
   after_create do |broadcast|
+    message_params = if broadcast.template.blank? || broadcast.template == 'text'
+                       { message: { text: broadcast.text } }
+                     elsif broadcast.template == 'image'
+                       {
+                         message: {
+                           attachment: {
+                             type: 'template',
+                             payload: {
+                               template_type: 'generic',
+                               elements: [
+                                 {
+                                   title: broadcast.title,
+                                   image_url: broadcast.image_url,
+                                   default_action: {
+                                     type: "web_url",
+                                     url: broadcast.link
+                                   },
+                                   buttons:[
+                                     {
+                                       type: "web_url",
+                                       url: broadcast.link,
+                                       title: broadcast.button_text
+                                     }
+                                   ]      
+                                 }
+                               ]
+                             }
+                           }
+                         }
+                       }
+                     else
+                       raise "Unknown broadcast message type: #{broadcast.template}"
+                     end
+
     User.all.each do |user|
       sent_at = Time.now
+      combined_message_params = { recipient: { id: user.fbid } }.merge(message_params)
       begin
-        Bot.deliver({
-          recipient: {
-            id: user.fbid
-          },
-          message: {
-            text: broadcast.text
-          }
-        }, access_token: ENV['ACCESS_TOKEN'])
+        Bot.deliver(combined_message_params, access_token: ENV['ACCESS_TOKEN'])
         user.broadcasts << broadcast
         user.last_message_sent_at = sent_at
         user.save!
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217161222) do
+ActiveRecord::Schema.define(version: 20170221141058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,8 +39,13 @@ ActiveRecord::Schema.define(version: 20170217161222) do
 
   create_table "broadcasts", force: :cascade do |t|
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "title"
+    t.string   "image_url"
+    t.string   "button_text"
+    t.string   "link"
+    t.string   "template"
   end
 
   create_table "broadcasts_users", id: false, force: :cascade do |t|
